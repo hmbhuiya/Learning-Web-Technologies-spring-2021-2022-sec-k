c1dd846b0157d6a51d4a34db5e2c6970695ac9b9
Hasan Mahmud Bhuiya <hasansakib028@gmail.com>
2022-02-09 11:43:31 +0600

    new php file added

---
+++ MID_LAB_TASK_04_PHP/Task6.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$student=array("American","International","University","Bangladesh");
+echo $student[1];
+echo("<br/>");
+
+
+if(in_array("American",$student)){
+echo "American is there inside the array";}
+
+else
+	{ echo "American is not there ";}
+
+
+?>
